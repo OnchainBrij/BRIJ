dde8a4f54b80a772497f6a6b1dd2ad7b4fa186ed
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-11-30 09:18:55 +0100

    .

--- brij/app/(app-layout)/explore-projects/page.jsx
+++ brij/app/(app-layout)/explore-projects/page.jsx
@@ -5,7 +5,7 @@ function ExploreProjects() {
   return (
     <div className="h-screen pl-5">
       <h1 className="font-bold mb-6">Explore Projects</h1>
-      <div className="grid grid-cols-3 2xl:grid-cols-4 gap-y-5">
+      <div className="grid grid-cols-3 2xl:grid-cols-4 gap-y-5 mb-6">
         {projects.map((item, index) => (
           <ProjectCard
             item={item}
